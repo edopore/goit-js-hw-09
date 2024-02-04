75a6acff839ca9505e1cb9a1bcca986cd8a90112
Eduardo Maya <ediij1994@gmail.com>
2024-02-03 21:47:54 -0500

    UPDATE tarea 1 realizada

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+buttons[0].addEventListener('click', event => {
+  intervalId = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+buttons[1].addEventListener('click', () => {
+  clearInterval(intervalId);
+});
