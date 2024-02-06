b9c18b0bb78edcc73ee9c6aa8223ff9de2c36018
Eduardo Maya <ediij1994@gmail.com>
2024-02-06 18:16:29 -0500

    tarea 3 comienza

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const startButton = document.querySelector('button');
+const valueInputs = document.querySelectorAll('input');
+
+let addDelay = valueInputs[0].valueAsNumber;
+console.log(addDelay);
+
+startButton.addEventListener('click', event => {
+  event.preventDefault();
+  for (let i = 0; i < valueInputs[2].valueAsNumber; i++) {
+    let promise = new Promise(createPromise);
+    promise.then((i + 1, valueInputs[1].valueAsNumber)).catch();
+    addDelay += valueInputs[1].valueAsNumber;
+    console.log(addDelay);
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    setTimeout(() => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }, delay);
   } else {
     // Reject
+
+    setTimeout(() => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
   }
 }
